8a3806dfd5dd53ab6a3510132d483b089388b744
Kiritchell <kiritchell@gmail.com>
2023-01-16 13:50:12 -0600

    index.html

---
+++ script.js
@@ -0,0 +1,4 @@
+function goToPayment(){window.location.href = 'https://www.paypal.com/us/home';}
+
+console.log("Viewport width: " + window.innerWidth);
+console.log("Viewport height: " + window.innerHeight);
