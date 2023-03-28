4608d79602c0bbc6ebe9b751fcd2d5731e273a1b
Kiritchell <kiritchell@gmail.com>
2023-03-27 22:00:38 -0500

    --all

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ function goToPayment(){window.location.href = '/giving.html';}
 //function goToPayment(){window.location.href = 'https://www.paypal.com/us/home';}
 console.log("Viewport width: " + window.innerWidth);
 console.log("Viewport height: " + window.innerHeight);
+
